153f84d73fb8709df7c0c2c83c4dc14add3a9e4f
Akhil Gupta <akhilgupta18ald@gmail.com>
2024-09-26 17:46:24 +0530

    Done

---
+++ 17-unixToTime.js
@@ -0,0 +1,17 @@
+//Write a JavaScript function to convert a Unix timestamp to time.
+
+function unixToTime(timestamp) {
+    let date = new Date(timestamp * 1000);
+    console.log(date.toString())
+    let hours = date.getHours();
+    // console.log(hours)
+    let minutes = date.getMinutes();
+    // console.log(minutes)
+    let seconds = date.getSeconds();
+    // console.log(seconds)
+    let formattedTime = hours + ":" + minutes + ":" + seconds;
+    // console.log(formattedTime)
+    return formattedTime;
+}
+
+console.log(unixToTime(1609459200))
