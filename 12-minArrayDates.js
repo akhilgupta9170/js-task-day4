538077b9887eef34b08c8a219b38b7d1bbbb8ffc
Akhil Gupta <akhilgupta18ald@gmail.com>
2024-09-26 16:20:00 +0530

    Done

---
+++ 12-minArrayDates.js
@@ -0,0 +1,15 @@
+//Write a JavaScript function to get the minimum date from an array of dates.
+
+
+function getMinimum(dates){
+    let min=  new Date(dates[0]);
+    for(let i=1; i<dates.length; i++){
+        let currentDate = new Date(dates[i]);
+            if(currentDate < min ){
+                min = currentDate;
+            }
+        
+    }
+ return min;
+}
+console.log(getMinimum(['2015/02/01', '2015/02/02', '2015/01/03']))
