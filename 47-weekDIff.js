2abe4a3aa9a194057f21b80ce046d13652877388
Akhil Gupta <akhilgupta18ald@gmail.com>
2024-09-27 19:17:48 +0530

    Done

---
+++ 47-weekDIff.js
@@ -0,0 +1,10 @@
+//Write a JavaScript function to get time differences in weeks between two dates.
+
+function getWeekDifference(date1, date2) {
+    let diffInMs = Math.abs(date2 - date1);
+    let diffInDays = diffInMs / (1000 * 60 * 60 * 24);
+    let diffInWeeks = Math.floor(diffInDays / 7);
+    return diffInWeeks;
+}
+
+console.log(getWeekDifference(new Date("October 13, 2014 08:11:00"), new Date("October 20, 2014 11:13:00")))
