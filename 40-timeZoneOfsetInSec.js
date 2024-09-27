f8e415796bc80daaedacac23ec9c79030edbd5c4
Akhil Gupta <akhilgupta18ald@gmail.com>
2024-09-27 18:20:24 +0530

    Done

---
+++ 40-timeZoneOfsetInSec.js
@@ -0,0 +1,8 @@
+//Write a JavaScript function to get the timezone offset in seconds.
+
+function getTimezoneOffsetInSeconds(date) {
+    let date1 = new Date(date);
+    return date1.getTimezoneOffset() * 60 * 1000;
+}
+
+console.log(getTimezoneOffsetInSeconds("2024-01-01T12:18:53"));
