48879055d71c33446881212ee8c2d3ea4fa1e182
Akhil Gupta <akhilgupta18ald@gmail.com>
2024-09-27 19:28:10 +0530

    Done

---
+++ 50-startweek.js
@@ -0,0 +1,13 @@
+//Write a JavaScript function to get the week's start date.
+
+function getWeekStartDate(date) {
+
+    let weekday = date.getDay();
+    let adjustedDate = new Date(date);
+    adjustedDate.setDate(adjustedDate.getDate() - weekday);
+
+    
+    return adjustedDate;
+}
+
+console.log(getWeekStartDate(new Date("2024-01-11")));
