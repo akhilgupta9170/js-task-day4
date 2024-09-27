f9a7f98f1a713b6545ea5a7a4bd8c6aecde4c8a0
Akhil Gupta <akhilgupta18ald@gmail.com>
2024-09-27 16:54:58 +0530

    Done

--- 08-diiffDates.js
+++ 08-diiffDates.js
@@ -1,18 +1,17 @@
 //Write a JavaScript function to get the difference between two dates in day
 
-function getDifferenceInDays(date1, date2) {
-     date1 = new Date(date1);
-     date2 = new Date(date2);
-    //  console.log(date1.getTime())
-    let diffInMilliseconds = Math.abs(date2.getTime() - date1.getTime());
-    let diffInDays = Math.ceil(diffInMilliseconds / (1000 * 60 * 60 * 24));
-    return diffInDays;
-}
-
-
-console.log(getDifferenceInDays('04/02/2014', '11/04/2014'));
-console.log(getDifferenceInDays('12/02/2014', '11/04/2014'));
-
+var is_date = function(input) {
+    
+    return input instanceof Date && !isNaN(input);
+  };
+  
+ 
+  console.log(is_date("October 13, 2014 11:13:00")); 
+  console.log(is_date(new Date(86400000)));
+  console.log(is_date(new Date(99, 5, 24, 11, 33, 30, 0))); 
+  console.log(is_date([1, 2, 4, 0]));
+  console.log(is_date(new Date("invalid date"))); 
+  
 
 
 
