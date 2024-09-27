e7206748851cb9d61d5f8b60124d9ab8d8b1bd80
Akhil Gupta <akhilgupta18ald@gmail.com>
2024-09-27 21:40:50 +0530

    Done

---
+++ 57-monthTOCalender.js
@@ -0,0 +1,16 @@
+//Write a JavaScript program to get the name of a day based on a given date in string format.
+function dayName(date){
+   if(typeof date !== 'string'){
+    return "Date is not a string"
+   }
+   let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+   let dt = date.split("/")
+//    console.log(dt)
+   let d = new Date(dt[2], dt[1] - 1, dt[0]);
+   return days[d.getDay()];
+
+
+}
+
+
+console.log(dayName("07/11/2000") )
