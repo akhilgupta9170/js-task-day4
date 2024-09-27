8bdb4c4172d9dedd6f7386cd447f1df9efa18f2c
Akhil Gupta <akhilgupta18ald@gmail.com>
2024-09-27 21:07:10 +0530

    Done

---
+++ 55-weekDayOrWeekEnd.js
@@ -0,0 +1,12 @@
+//Write a JavaScript program to check if the current date is a weekday or a weekend.
+function weekEndOrWeekday(date){
+    let day=date.getDay();
+    if(day===0 || day===6){
+        return "Weekend";
+    }else{
+        return "Weekday";
+    }
+
+}
+
+console.log(weekEndOrWeekday(new Date("2024-01-21")));
