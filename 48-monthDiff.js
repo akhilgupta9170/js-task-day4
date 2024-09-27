08a74b8557c81ac6d4d52a764866f574b11dd4ea
Akhil Gupta <akhilgupta18ald@gmail.com>
2024-09-27 19:20:45 +0530

    Done

---
+++ 48-monthDiff.js
@@ -0,0 +1,9 @@
+//Write a JavaScript function to get time differences in months between two dates.
+
+function getMonthDifference(date1, date2) {
+    let diffInMs = Math.abs(date2 - date1);
+    let diffInMonths = Math.floor(diffInMs / (1000 * 60 * 60 * 24*30));
+    return diffInMonths;
+}
+
+console.log(getMonthDifference(new Date("October 13, 2014 08:11:00"), new Date("December 20, 2014 11:13:00")))
